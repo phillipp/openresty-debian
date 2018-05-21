803843e6fde020b233737dc095ea2f27c5dd4bb2
Phillipp Röll <phillipp.roell@trafficplex.de>
2018-05-21 23:10:43 +0200

    Add script to automatically build current version

---
+++ run.rb
@@ -0,0 +1,12 @@
+require 'httparty'
+require 'pry'
+
+response = HTTParty.get('https://openresty.org/en/download.html')
+versions = response.body
+                   .scan(/openresty\-([0-9\.]+)\.([0-9]+)\.tar\.gz/)
+                   .uniq
+
+v = versions.first
+
+puts "DEB_MAJOR #{v[0]} DEB_MINOR #{v[1]} bash ./build"
+system({ 'DEB_MAJOR' => v[0], 'DEB_MINOR' => v[1] }, 'bash ./build')
